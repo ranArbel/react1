c1a1d4a4465fc6bdb87f3513e679a531b3db4b99
ranArbel <ranarbel333@gmail.com>
2020-02-16 19:20:07 +0200

    test

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {HashRouter as Router,Switch,Route,Link} from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Router>
+      <Link to='/'>HomePage</Link>
+      <Link to='/page2'>2</Link>
+      <Switch>
+        <Route exact path='/' component={()=>{return <h1>HomePage</h1>}}/>
+        <Route exact path='/page2' component={()=>{return <h1>Page2</h1>}}/>
+
+      </Switch>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
